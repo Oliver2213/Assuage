4f67593c851d4ecf677a9c04fa0981b1502054ad
Blake Oliver <oliver22213@me.com>
2026-07-03 09:58:52 -0600

    Initial xcode template commit

---
+++ Cypherdex/CypherdexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CypherdexApp.swift
+//  Cypherdex
+//
+//  Created by Blake Oliver on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CypherdexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
